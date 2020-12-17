32c7cc58570bd31b7a566512036cf1480be05a02
leosant <leonardosantos45548@gmail.com>
2020-12-16 23:50:22 -0300

    Commit da aplicação de manipulacao de arquivos de forma manual

---
+++ ManipulandoArquivos/src/br/com/tecleo/main/Main.java
@@ -0,0 +1,45 @@
+package br.com.tecleo.main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String path = "C:\\Users\\LeoUser\\Documents\\Teste_Desenvolvimento\\Textos\\teste.txt";
+		
+		BufferedReader br = null;
+		FileReader fr = null;
+		
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			//br = new BufferedReader(new FileReader(path));
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error: "+ e.getMessage());
+		} finally {
+			try {
+				
+				if(br != null) {
+					br.close();
+				}
+				if(fr != null) {
+					fr.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
